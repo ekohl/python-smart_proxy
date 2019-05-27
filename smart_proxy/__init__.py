05286ca9a59155cac4ed08238c083fa9228d86b2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-05-27 13:20:24 +0200

    Empty features

--- smart_proxy/__init__.py
+++ smart_proxy/__init__.py
@@ -4,3 +4,9 @@ from starlette.responses import JSONResponse
 
 
 app = Starlette()  # pylint: disable=invalid-name
+
+
+@app.route('/features')
+async def features(request):
+    result = []
+    return JSONResponse(result)

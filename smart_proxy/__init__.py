99ca4b44723ab251b1e358a911cb3836db97de11
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-05-27 13:21:31 +0200

    Empty version

--- smart_proxy/__init__.py
+++ smart_proxy/__init__.py
@@ -10,3 +10,14 @@ app = Starlette()  # pylint: disable=invalid-name
 async def features(request):
     result = ['puppet']
     return JSONResponse(result)
+
+
+@app.route('/version')
+async def version(request):
+    result = {
+        'version': '0.1.0',
+        'modules': {
+            'puppet': '0.1.0',
+        },
+    }
+    return JSONResponse(result)
